84876b33a7d166b7552bd6fdd597a9ad2ed37baa
neropsys <neropsys@gmail.com>
2015-11-28 17:32:07 +0900

    Added map to view

--- RailroProject/mainwindow.cpp
+++ RailroProject/mainwindow.cpp
@@ -6,8 +6,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-}
+    ui->graphicsView->setStyleSheet("background-image:url(:/Image/Map_of_South_Korea-blank.png)");
+    QImage bg(":/Image/Map_of_South_Korea-blank.png");
 
+    int height = bg.height();
+    int width = bg.width();
+    ui->graphicsView->setFixedSize(width, height);
+    centralWidget()->setFixedSize(width, height);
+    this->setFixedSize(width, height);
+
+}
 MainWindow::~MainWindow()
 {
     delete ui;
